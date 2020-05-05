e373e7130a072e58dadd264a20b4c458b4ad183f
TheRoxas <aleksander.oliveira2@etec.sp.gov.br>
2020-05-05 20:15:41 -0300

    Criei uma biblioteca que repete os Olás

--- principal.cpp
+++ principal.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-
+#include "variosOlas.h"
 int main(){
 	std::cout<< "Olá Mundo" << std::endl;
 	std::cout<< "Sem porteiras" << std::endl;
+	olas(100);
 }
---
+++ variosOlas.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+void variosOlas(unsigned int qtde){
+	for(int i=0; i< qtde; i++){
+		std::cout<<"Olá mundo" << std:endl;
+	}
+}
